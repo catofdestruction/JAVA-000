c8281f611d1185c19ca0e94546ea62bc0daa767f
wangxinyu13 <wangxinyu13@baidu.com>
2020-11-26 00:23:01 +0800

    init tables ddl

--- Week_06/README.md
+++ Week_06/README.md
@@ -1,1 +1,40 @@
 学习笔记
+
+-- TODO: to be optimized
+
+-- auto-generated definition by idea
+create table t_user
+(
+    id            bigint auto_increment primary key,
+    account       varchar(30) not null,
+    identity_card varchar(18) not null,
+    name          varchar(30) not null,
+    password      varchar(20) not null,
+    phone         varchar(11) not null
+);
+
+-- auto-generated definition by idea
+create table t_order
+(
+    id                 bigint auto_increment primary key,
+    created_date       bigint       not null,
+    last_modified_date bigint       not null,
+    seller_id          bigint       not null,
+    status             varchar(255) not null,
+    total_price        varchar(20)  not null,
+    buyer_id           bigint       not null,
+    constraint FKk26ipfwqtjq9995jrn29murlt
+        foreign key (buyer_id) references t_user (id)
+);
+
+-- auto-generated definition by idea
+create table t_merchandise
+(
+    id       bigint auto_increment primary key,
+    name     varchar(30) not null,
+    price    varchar(10) not null,
+    weight   varchar(10) null,
+    order_id bigint      not null,
+    constraint FK3it2cp3fx6v6cqo4bp3fnqjuc
+        foreign key (order_id) references t_order (id)
+);
--- Week_06/mysql-demo/src/main/resources/db/dbinit.sql
+++ Week_06/mysql-demo/src/main/resources/db/dbinit.sql
@@ -1,7 +1,7 @@
-
 -- 创建数据库jddb:
 DROP DATABASE IF EXISTS jddb;
-CREATE DATABASE jddb;``
+CREATE DATABASE jddb;
+
 
 -- 创建登录用户jd/口令jd
 CREATE USER IF NOT EXISTS 'jd'@'%' IDENTIFIED BY 'jd';
@@ -10,6 +10,44 @@ FLUSH PRIVILEGES;
 
 -- 创建表:
 USE jddb;
+
+-- TODO: to be optimized
+-- auto-generated definition
+create table t_user
+(
+    id            bigint auto_increment primary key,
+    account       varchar(30) not null,
+    identity_card varchar(18) not null,
+    name          varchar(30) not null,
+    password      varchar(20) not null,
+    phone         varchar(11) not null
+);
+
+-- auto-generated definition
+create table t_order
+(
+    id                 bigint auto_increment primary key,
+    created_date       bigint       not null,
+    last_modified_date bigint       not null,
+    seller_id          bigint       not null,
+    status             varchar(255) not null,
+    total_price        varchar(20)  not null,
+    buyer_id           bigint       not null,
+    constraint FKk26ipfwqtjq9995jrn29murlt
+        foreign key (buyer_id) references t_user (id)
+);
+
+-- auto-generated definition
+create table t_merchandise
+(
+    id       bigint auto_increment primary key,
+    name     varchar(30) not null,
+    price    varchar(10) not null,
+    weight   varchar(10) null,
+    order_id bigint      not null,
+    constraint FK3it2cp3fx6v6cqo4bp3fnqjuc
+        foreign key (order_id) references t_order (id)
+);
 
 
 /**
