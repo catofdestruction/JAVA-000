ae6b931cfc84bc8e6009a0a4b996ffbc0908af9d
wangxinyu13 <wangxinyu13@baidu.com>
2020-12-02 20:25:35 +0800

    fix order of hibernate entity

--- Week_07/mysql-demo/src/main/resources/db/dbinit.sql
+++ Week_07/mysql-demo/src/main/resources/db/dbinit.sql
@@ -13,38 +13,37 @@ USE jddb;
 
 create table t_user
 (
-    id            bigint auto_increment
-        primary key,
+    id            bigint auto_increment primary key,
     account       varchar(30)                         not null,
-    birthday      datetime                            null,
-    gender        tinyint(1)                          not null,
-    identity_card varchar(19)                         not null,
-    level         tinyint(1)                          not null,
-    nick_name     varchar(30)                         null,
     password      varchar(20)                         not null,
-    phone         varchar(11)                         not null,
+    nick_name     varchar(30)                         null,
+    gender        tinyint(1)                          not null,
     real_name     varchar(10)                         not null,
-    register_time timestamp default CURRENT_TIMESTAMP not null
+    phone         varchar(11)                         not null,
+    identity_card varchar(19)                         not null,
+    register_time timestamp default CURRENT_TIMESTAMP not null,
+    birthday      datetime                            null,
+    level         tinyint(1)                          not null
 );
 
 create table t_order
 (
     id                 bigint auto_increment primary key,
-    address_id         bigint(19)                          not null,
-    created_date       timestamp default CURRENT_TIMESTAMP not null,
-    delivery_sn        varchar(64)                         null,
-    freight_amount     decimal(10, 2)                      not null,
-    gender             tinyint(1)                          not null,
-    is_deleted         bit                                 not null,
-    last_modified_date timestamp default CURRENT_TIMESTAMP not null,
     order_sn           varchar(64)                         not null,
-    pay_amount         decimal(10, 2)                      not null,
-    pay_type           tinyint(1)                          not null,
-    remark             varchar(255)                        null,
-    status             tinyint(1)                          not null,
-    total_amount       decimal(10, 2)                      not null,
     user_id            bigint(19)                          not null,
-    user_name          varchar(10)                         not null
+    user_name          varchar(10)                         not null,
+    total_amount       decimal(10, 2)                      not null,
+    freight_amount     decimal(10, 2)                      not null,
+    pay_amount         decimal(10, 2)                      not null,
+    gender             tinyint(1)                          not null,
+    pay_type           tinyint(1)                          not null,
+    status             tinyint(1)                          not null,
+    address_id         bigint(19)                          not null,
+    delivery_sn        varchar(64)                         null,
+    remark             varchar(255)                        null,
+    is_deleted         bit                                 not null,
+    created_date       timestamp default CURRENT_TIMESTAMP not null,
+    last_modified_date timestamp default CURRENT_TIMESTAMP not null
 );
 
 /**
