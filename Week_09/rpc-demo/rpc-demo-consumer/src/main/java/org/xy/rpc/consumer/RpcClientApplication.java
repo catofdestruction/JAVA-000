b7afbc13d7e061c40712bc43b15781277e5d396d
wangxinyu13 <wangxinyu13@baidu.com>
2020-12-16 23:21:18 +0800

    test RpcJdkProxyFactory

--- Week_09/rpc-demo/rpc-demo-consumer/src/main/java/org/xy/rpc/consumer/RpcClientApplication.java
+++ Week_09/rpc-demo/rpc-demo-consumer/src/main/java/org/xy/rpc/consumer/RpcClientApplication.java
@@ -2,6 +2,9 @@ package org.xy.rpc.consumer;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.xy.rpc.api.model.User;
+import org.xy.rpc.api.service.UserService;
+import org.xy.rpc.core.client.RpcJdkProxyFactory;
 
 /**
  * RpcClientApplication
@@ -13,9 +16,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages = {"org.xy.rpc.consumer"})
 public class RpcClientApplication {
 
+    private static final String HOST_URL = "http://localhost:8167/";
+
     public static void main(String[] args) {
         log.info("{}", RpcClientApplication.class);
 
+        UserService userService = RpcJdkProxyFactory.getProxy(UserService.class, HOST_URL);
+        User user = userService.findUserById(1);
+        log.info("user {}", user);
 
+//        OrderService orderService = RpcJdkProxyFactory.getProxy(OrderService.class, HOST_URL);
+//        orderService.findOrderById(1992129);
     }
 }
--- Week_09/rpc-demo/rpc-demo-provider/src/main/java/org/xy/rpc/provider/RpcServerApplication.java
+++ Week_09/rpc-demo/rpc-demo-provider/src/main/java/org/xy/rpc/provider/RpcServerApplication.java
@@ -1,8 +1,16 @@
 package org.xy.rpc.provider;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.xy.rpc.api.model.User;
+import org.xy.rpc.core.api.RpcRequest;
+import org.xy.rpc.core.api.RpcResponse;
 
 /**
  * RpcServerApplication
@@ -11,11 +19,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2020/12/16
  */
 @Slf4j
+@RestController
 @SpringBootApplication(scanBasePackages = {"org.xy.rpc.provider"})
 public class RpcServerApplication {
 
 	public static void main(String[] args) {
+		log.info("{}", RpcServerApplication.class);
 		SpringApplication.run(RpcServerApplication.class, args);
 	}
 
+	@PostMapping("/")
+	public RpcResponse invoke(@RequestBody RpcRequest request) {
+		log.info("receive request ===> {}", request);
+		return new RpcResponse(JSON.toJSONString(new User(1, "go"), SerializerFeature.WriteClassName), true, null);
+	}
 }
